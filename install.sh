96bf34cdbcfa35adb2387e8108a35170312e516f
root <gwleong@gmail.com>
2016-03-14 18:53:42 +0000

    Automatically committed by jiffy

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ PKGNAME=$1
 mkdir /var/tmp/jiffy/temp
 tar xvfz /var/tmp/jiffy/$PKGNAME -C /var/tmp/jiffy/temp && \
 ln -s /var/tmp/jiffy/temp /var/jiffy-run && \
+cd /var/jiffy-run
 /var/jiffy-run/run_order && \
 rm -rf /var/jiffy-run && \
 rm -rf /var/tmp/jiffy/temp
